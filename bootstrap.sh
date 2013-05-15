bb21d1c146cbe62a582fd1ca6d629de5145c1289
vagrant <vagrant@lucid32.hsd1.ca.comcast.net>
2013-05-15 04:24:46 +0200

    restructuring directories

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-apt-get update
-apt-get -y install git-core irb rubygems libopenssl-ruby ruby1.8-dev rake build-essential
-
-gem install json
-gem install -v 1.3.6 rubygems-update && ruby `gem env gemdir`/gems/rubygems-update-1.3.6/setup.rb 
-gem install infusionsoft
